3b1a0b1ff9bdb908e7ea1722f471e22d280f3105
RealRahan <103349690+RealRahan@users.noreply.github.com>
2022-08-17 21:00:10 +0300

    Update __init__.py

--- DiscordUtils/__init__.py
+++ DiscordUtils/__init__.py
@@ -3,6 +3,6 @@ from DiscordUtils.InviteTracker import InviteTracker
 from DiscordUtils.Music import Music
 
 __title__ = "DiscordUtils"
-__version__ = "1.3.4"
-__author__ = "toxicrecker"
+__version__ = "1.7.4"
+__author__ = "rahan"
 __license__ = "MIT"
